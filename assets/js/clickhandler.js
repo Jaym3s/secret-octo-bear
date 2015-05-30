04551cec6da2a48bac69cf501fb7c4c22facfbe3
Jaymes Waters <Jaym3s@gmail.com>
2015-05-30 15:29:45 -0500

    Content updates

--- assets/js/clickhandler.js
+++ assets/js/clickhandler.js
@@ -3,7 +3,7 @@
   var links = document.getElementsByClassName("links"),
   templateDiv = document.getElementById("template"),
   nav = document.querySelector('nav > ul'),
-  clicked = false, viewCount = { home: 2, about: 1, contact: 1 };
+  clicked = false, data = { };
 
   function clickHandler(e) {
     var target = event.target,
@@ -17,10 +17,9 @@
     target.parentElement.setAttribute('class','active');
     clicked = target.parentElement;
 
-    dust.render(templateName, viewCount, function (err, out) {
+    dust.render(templateName, data, function (err, out) {
       if(err) console.log('Error:',err);
       else {
-        viewCount[templateName]++;
         templateDiv.innerHTML = out;
       }
     });
